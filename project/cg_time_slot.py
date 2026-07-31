dadf91f6c195a20c2ba8dd0b209f2411d0e5f976
Greg Skluzacek <Greg.Skluzacek@nielseniq.com>
2026-07-30 22:32:50 -0500

    migrated from avails/Avail/Availability to time_slots/TimeSlot/Time Slots
    missed a couple of files in the commit

--- project/cg_time_slot.py
+++ project/cg_time_slot.py
@@ -42,22 +42,22 @@ logger = logging.getLogger("ksbot.timeslot")
 
 class TimeSlot(app_commands.Group):
     def __init__(self):
-        super().__init__(name="timeslot", description="Kingshot Player Time Slot Tools")
+        super().__init__(name="timeslot", description="Kingshot Player available Time Slot Tools")
 
     # /timeslot add: inserts a record into the time_slots table.
     #   uses the active player and event stored in the player session state.
     #   user must specify
     #       - tslot_type:     prefered, acceptable, avoid
-    #       - start_time:     This is in the user's LOCAL time. It time value only, in 24 hour format.
+    #       - start_time:     This is in the user's LOCAL time. It is only the time value, in 24 hour format.
     #                         It is specified in 15 minute increments, e.g. 14:00, 14:15, 14:30, 14:45, 15:00, etc.
     #       - end_time:       see start_time details.
     #       - priority:       integer
-    #   validated_ind:        defaults to 1 (true), the scheduling admin can set this to 0 (false) and ask the player to confirm their time slot is accruate.
+    #   validated_ind:        defaults to 1 (true), the scheduling admin can set this to 0 (false) and ask the player to confirm their time slot is accurate.
     #   create_date_time:     set to the current timestamp
     #   update_date_time:
     #   create_account_id:    set to the account_id of the person executing the command
     #   update_account_id:
-    @app_commands.command(name="add", description="Add a Time Slot for a Kingshot Player")
+    @app_commands.command(name="add", description="Add an available Time Slot for a Kingshot Player")
     async def add(
             self,
             interaction: discord.Interaction,
@@ -71,7 +71,7 @@ class TimeSlot(app_commands.Group):
         await interaction.response.send_message("✅ time slot added", ephemeral=True)
 
     # /timeslot edit: updates a record in the time_slots table.
-    @app_commands.command(name="edit", description="Edit a Time Slot for a Kingshot Player")
+    @app_commands.command(name="edit", description="Edit an available Time Slot for a Kingshot Player")
     async def edit(
             self,
             interaction: discord.Interaction,
@@ -82,7 +82,7 @@ class TimeSlot(app_commands.Group):
         await interaction.response.send_message("✅ time slot updated", ephemeral=True)
 
     # /timeslot remove: delete a record from the time_slots table.
-    @app_commands.command(name="remove", description="Removes a Time Slot for a Kingshot Player")
+    @app_commands.command(name="remove", description="Removes an available Time Slot for a Kingshot Player")
     async def remove(
             self,
             interaction: discord.Interaction,
@@ -93,7 +93,7 @@ class TimeSlot(app_commands.Group):
         await interaction.response.send_message("✅ time slot deleted", ephemeral=True)
 
     # /timeslot list: lists all records in the time_slots table for the active player and event
-    @app_commands.command(name="list", description="List Time Slots for a Kingshot Player")
+    @app_commands.command(name="list", description="List available Time Slots for a Kingshot Player")
     async def list(
             self,
             interaction: discord.Interaction,
@@ -103,7 +103,7 @@ class TimeSlot(app_commands.Group):
         await interaction.response.send_message("✅ time slots listed", ephemeral=True)
 
     # /timeslot show: shows a specific record in the time_slots table for the active player and event
-    @app_commands.command(name="show", description="Show a Time Slot for a Kingshot Player")
+    @app_commands.command(name="show", description="Show an available Time Slot for a Kingshot Player")
     async def show(
             self,
             interaction: discord.Interaction,
