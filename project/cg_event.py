d65156f330e35f8ab639a6103a64e3112badca9f
Greg Skluzacek <Greg.Skluzacek@nielseniq.com>
2026-08-01 14:47:07 -0500

    added stub for event activate slash commad.

--- project/cg_event.py
+++ project/cg_event.py
@@ -1,4 +1,19 @@
-# TODO: add /event activate to set the active event id for adding time slots.
-#       Currently setting the active event id to 1 in the account create command.
+import discord
+from discord import app_commands
+
 
 # TODO: add a event command to initalize a default set of events.
+
+class Event(app_commands.Group):
+    def __init__(self):
+        super().__init__(name="event", description="Kingshot Player Event Tools")
+
+    # TODO: add /event activate to set the active event id for adding time slots.
+    #       Currently setting the active event id to 1 in the account create command.
+
+    async def activate(
+            self,
+            interaction: discord.Interaction,
+            event_id: int
+    ):
+        ...
